54c71d79dfd3ba18016606d9fc380dda4f60f320
Uladzislau Krasny <uladzislau.krasny@itechart-group.com>
2019-06-11 17:05:22 +0300

    Added errors handling for incorrect command line arguments.

---
+++ index.js
@@ -0,0 +1,43 @@
+const CLI_NAME = 'csv-to-json-cli';
+const HELP_FLAG = '--help';
+const SOURCE_FILE_FLAG = '--sourceFile';
+const RESULT_FILE_FLAG = '--resultFile';
+const SEPARATOR_FLAG = '--separator';
+const usageGuide = `
+Usage: ${CLI_NAME} ${SOURCE_FILE_FLAG} value ${RESULT_FILE_FLAG} value [${SEPARATOR_FLAG}] [value]
+
+    ${SOURCE_FILE_FLAG} - Path to the csv file that need to be converted.
+    ${RESULT_FILE_FLAG} - Path to the result json file.
+    ${SEPARATOR_FLAG}  - Separator that is used while converting. If not specified it will be automatically detected.
+`;
+
+const [
+    sourceFileArg,
+    sourceFilePath,
+    resultFileArg,
+    resultFilePath,
+    separatorArg,
+    separator
+] = process.argv.slice(2);
+
+(async () => {
+    try {
+        if (sourceFileArg === undefined || sourceFileArg === HELP_FLAG) {
+            throw new Error(usageGuide);
+        } else if (sourceFileArg !== SOURCE_FILE_FLAG) {
+            throw new Error(`Error: Wrong command line argument ${sourceFileArg} expected ${SOURCE_FILE_FLAG}`);
+        }
+
+        if (resultFileArg === undefined) {
+            throw new Error(usageGuide);
+        } else if (resultFileArg !== RESULT_FILE_FLAG) {
+            throw new Error(`Error: Wrong command line argument ${resultFileArg} expected ${RESULT_FILE_FLAG}`);
+        }
+
+        if (separatorArg !== undefined && separatorArg !== SEPARATOR_FLAG) {
+            throw new Error(`Error: Wrong command line argument ${separatorArg} expected ${SEPARATOR_FLAG}`);
+        }
+    } catch (err) {
+        console.error(`${err.message}`);
+    }
+})();
