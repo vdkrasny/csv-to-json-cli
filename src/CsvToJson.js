167a06ff4d4bad4b5c0578fa8dae4c9de025b7cd
Uladzislau Krasny <uladzislau.krasny@itechart-group.com>
2019-06-11 18:03:03 +0300

    Added feature to detect separator automatically.

--- src/CsvToJson.js
+++ src/CsvToJson.js
@@ -6,6 +6,10 @@ module.exports = class {
     }
 
     setKeys(str) {
+        if (!this.separator) {
+            this.separator = this._detectSeparator(str);
+        }
+
         this._keys = str.split(this.separator);
     }
 
@@ -16,5 +20,38 @@ module.exports = class {
         this._keys.forEach((key, idx) => { object[key] = values[idx]; });
 
         return object;
+    }
+
+    _detectSeparator(str) {
+        const separators = [',', ';', '|', '\t', ' '];
+
+        const detectedSeparator = {
+            separator: null,
+            matchCount: 0
+        };
+
+        separators.forEach((separator) => {
+            let previous = null;
+            let current = null;
+            let next = null;
+            let matchCount = 0;
+
+            for (let i = 0; i < str.length; i += 1) {
+                previous = str[i - 1];
+                current = str[i];
+                next = str[i + 1];
+
+                if (current === separator && previous !== separator && next !== separator) {
+                    matchCount += 1;
+                }
+            }
+
+            if (detectedSeparator.matchCount < matchCount) {
+                detectedSeparator.separator = separator;
+                detectedSeparator.matchCount = matchCount;
+            }
+        });
+
+        return detectedSeparator.separator;
     }
 };
