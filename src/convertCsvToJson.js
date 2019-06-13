de1b86268447bc5079c965b63bd25dba49369994
Uladzislau Krasny <uladzislau.krasny@itechart-group.com>
2019-06-13 16:54:59 +0300

    Added streams errors handlers.

--- src/convertCsvToJson.js
+++ src/convertCsvToJson.js
@@ -4,13 +4,21 @@ const CsvToJsonStream = require('./CsvToJsonStream');
 module.exports = (csvFilePath, jsonFilePath, separator) => new Promise((resolve, reject) => {
     console.log('The conversion process is started...');
 
-    fs.createReadStream(csvFilePath)
-        .on('error', err => reject(err))
-        .pipe(new CsvToJsonStream(separator))
-        .pipe(fs.createWriteStream(jsonFilePath))
-        .on('finish', () => {
-            console.log(`The file ${csvFilePath} has been successfully converted to ${jsonFilePath}`);
+    const csvReadStream = fs.createReadStream(csvFilePath, { encoding: 'utf8' });
+    const csvToJsonStream = new CsvToJsonStream(separator);
+    const jsonWriteStream = fs.createWriteStream(jsonFilePath, { encoding: 'utf8' });
 
-            return resolve();
-        });
+    csvReadStream.on('error', err => reject(err));
+    csvToJsonStream.on('error', err => reject(err));
+    jsonWriteStream.on('error', err => reject(err));
+
+    const converterStream = csvReadStream
+        .pipe(csvToJsonStream)
+        .pipe(jsonWriteStream);
+
+    converterStream.on('finish', () => {
+        console.log(`The file ${csvFilePath} has been successfully converted to ${jsonFilePath}`);
+
+        return resolve();
+    });
 });
